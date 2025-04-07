69e75cb56c913bf332e8d1af9df5fbd55c2e03b6
Szymon Kozlowski <szymonkozlowski1092@gmail.com>
2025-04-07 18:57:51 +0200

    Implement Method GetChats in UserService and UserController

---
+++ MessageAppBackend/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using MessageAppBackend.Database;
+using MessageAppBackend.DbModels;
+using MessageAppBackend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageAppBackend.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly MessageAppDbContext _dbContext;
+        public UserService(MessageAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Chat>> GetChats(Guid userId)
+        {
+            var chats = await _dbContext.Chats
+                .Where(c => c.Users!
+                .Any(uc => uc.UserId == userId))
+                .Include(c => c.Messages)
+                .Include(c => c.Users)
+                .ToListAsync();
+
+            return chats!;
+        }
+    }
+}
